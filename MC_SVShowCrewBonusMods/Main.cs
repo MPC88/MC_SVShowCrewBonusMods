9808e646d607c4fdaa552fe09a3e69674dfa8e22
MPC88 <myemail@gmail.com>
2023-04-10 08:54:30 +0100

    Reworked display  to +/- format.

--- MC_SVShowCrewBonusMods/Main.cs
+++ MC_SVShowCrewBonusMods/Main.cs
@@ -14,7 +14,7 @@ namespace MC_SVNoAESound
     {
         public const string pluginGuid = "mc.starvalor.showcrewbonusmods";
         public const string pluginName = "SV Show Crew Bonus Mods";
-        public const string pluginVersion = "1.0.0";
+        public const string pluginVersion = "1.0.1";
 
         public void Awake()
         {
@@ -25,7 +25,9 @@ namespace MC_SVNoAESound
         [HarmonyPostfix]
         private static void SSB_GetStrPost(SkillShipBonus __instance, ref string __result)
         {
-            __result += " (" + __instance.modifier * 100 + "%)";
+            float mod = __instance.modifier - 1.0f;
+            string sym = mod < 0 ? "" : "+";
+            __result += " (" + sym + (mod * 100) + "%)";
         }
     }
 }
