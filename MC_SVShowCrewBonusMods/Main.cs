71397b3f941b1615e8f789cd487ca770a2b4b32f
MPC88 <myemail@gmail.com>
2023-04-24 08:48:29 +0100

    Small update to show prodigy bonus if it's in effect.

--- MC_SVShowCrewBonusMods/Main.cs
+++ MC_SVShowCrewBonusMods/Main.cs
@@ -14,7 +14,7 @@ namespace MC_SVNoAESound
     {
         public const string pluginGuid = "mc.starvalor.showcrewbonusmods";
         public const string pluginName = "SV Show Crew Bonus Mods";
-        public const string pluginVersion = "1.0.2";
+        public const string pluginVersion = "1.0.3";
 
         public void Awake()
         {
@@ -23,11 +23,20 @@ namespace MC_SVNoAESound
 
         [HarmonyPatch(typeof(SkillShipBonus), nameof(SkillShipBonus.GetString))]
         [HarmonyPostfix]
-        private static void SSB_GetStrPost(SkillShipBonus __instance, ref string __result)
+        private static void SSB_GetStrPost(SkillShipBonus __instance, SpaceShip ss, bool secondInCommand, bool thirdInCommand, ref string __result)
         {
             float mod = __instance.modifier - 1.0f;
+            string colOpen = "";
+            string colClose = "";
+            if (ss != null && ss.crew.isPlayer && PChar.Char.SK[47] == 1 && (secondInCommand || (thirdInCommand && ss.crew.prodigyAffects3rdSeat)))
+            {
+                mod = 0.5f;
+                colOpen = ColorSys.infoPos;
+                colClose = "</color>";
+            }
+            
             string sym = mod < 0 ? "" : "+";
-            __result += " (" + sym + (Math.Round(mod,2) * 100) + "%)";
+            __result += " (" + colOpen + sym + (Math.Round(mod,2) * 100) + "%" + colClose + ")";
         }
     }
 }
